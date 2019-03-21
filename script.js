bda26e037ef787dfb110d2ea736319202cf717e1
bioHazard33 <pahulkalra33viii@gmail.com>
2019-03-21 20:19:20 +0530

    FOOTER

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function search(){
 	var s=$(".form-inline");
 	s.toggleClass("search-display");
 }
+
 $("#search").keypress(function(event) {
 	if(keyCode==13 && $("#search").val()!=null && $("#search".val()!=' '))	 document.getElementById('search').submit();
+});
+
+$("#feedback").keypress(function(event) {
+	if(keyCode==13 && $("#feedback").val()!=null && $("#feedback".val()!=' '))	 document.getElementById('feedback').submit();
+});
+
+$("#newsletter").keypress(function(event) {
+	if(keyCode==13 && $("#newsletter").val()!=null && $("#newsletter".val()!=' '))	 document.getElementById('newsletter	').submit();
 });
