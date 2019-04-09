1e8e24fbd4c86d388010b819786d1cf507e8d58f
bioHazard33 <pahulkalra33viii@gmail.com>
2019-04-09 22:32:07 +0530

    LOGIN etc..

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function search(){
-	var s=$(".form-inline");
+	let s=$(".form-inline");
 	s.toggleClass("search-display");
 }
 
@@ -7,10 +7,17 @@ $("#search").keypress(function(event) {
 	if(keyCode==13 && $("#search").val()!=null && $("#search".val()!=' '))	 document.getElementById('search').submit();
 });
 
+$(document).keydown(function(e){
+if (e.keyCode==27){
+	let s=$(".form-inline");
+    s.removeClass("search-display");
+	}
+});
+
 $("#feedback").keypress(function(event) {
-	if(keyCode==13 && $("#feedback").val()!=null && $("#feedback".val()!=' '))	 document.getElementById('feedback').submit();
+	if(keyCode==13 && $("#feedback").val()!='' && $("#feedback".val()!=' '))	 document.getElementById('feedback').submit();
 });
 
 $("#newsletter").keypress(function(event) {
-	if(keyCode==13 && $("#newsletter").val()!=null && $("#newsletter".val()!=' '))	 document.getElementById('newsletter	').submit();
+	if(keyCode==13 && $("#newsletter").val()!='' && $("#newsletter".val()!=' '))	 document.getElementById('newsletter').submit();
 });
